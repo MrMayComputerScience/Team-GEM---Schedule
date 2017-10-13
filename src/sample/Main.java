227335a1de54d4cb66ca72b276f3ea3f7152d112
Bondzberg <32276521+Bondzberg@users.noreply.github.com>
2017-10-13 14:30:27 -0500

    Day 1-Choose Printer and Pinter

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -7,16 +7,16 @@ import javafx.print.Printer;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.shape.Circle;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.print.PrinterJob;
+
 
 import java.io.*;
 import java.util.ArrayList;
@@ -35,7 +35,9 @@ public class Main extends Application
         bp = new BorderPane();
         root.getChildren().add(bp);
         primaryStage.setScene(new Scene(root, 300, 275));
-
+        //print(new Circle(100));
+        Printer x = Printer.getDefaultPrinter();
+        choosePrinter(x);
         primaryStage.show();
     }
     public void fileChooser()
@@ -44,8 +46,9 @@ public class Main extends Application
     }
     public void print(Node x)
     {
-
-        PrinterJob job = PrinterJob.createPrinterJob(choosePrinter());
+        Printer y = Printer.getDefaultPrinter();
+        choosePrinter(y);
+        PrinterJob job = PrinterJob.createPrinterJob();
         if (job != null) {
             boolean success = job.printPage(x);
             if (success) {
@@ -53,11 +56,33 @@ public class Main extends Application
             }
         }
     }
-    public Printer choosePrinter()
+    public void choosePrinter(Printer print)
     {
+        ChoiceBox<Printer> t = new ChoiceBox();
 
 
-        return Printer.getDefaultPrinter();
+        Button butt = new Button();
+        Stage printer = new Stage();
+        StackPane root = new StackPane();
+
+        t.getItems().add(Printer.getDefaultPrinter());
+
+        for (Printer x : Printer.getAllPrinters()) {
+            t.getItems().add(x);
+        }
+        //t = Printer.getAllPrinters();
+        root.getChildren().add(t);
+        printer.setScene(new Scene(root, 300, 275));
+        printer.show();
+        root.getChildren().add(butt);
+        butt.setOnAction((ActionEvent e) -> {
+            printer.close();
+
+        });
+        if(butt.isPressed()) {
+            print = t.getValue();
+        }
+
     }
     public void readFile()
     {
